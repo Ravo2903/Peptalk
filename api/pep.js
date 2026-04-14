090b1291e80e180719e6f5c0ccd1d2ddba3cac09
Ravo2903 <arnesen.cad@gmail.com>
2026-04-14 11:31:34 +0200

    Update pep.js

--- api/pep.js
+++ api/pep.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -67,4 +67,4 @@ Svar på norsk.`;
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
